f9383d006a09b8473b24d7e9850db590962a1fe5
OKW32 <ongkanwu32@gmail.com>
2025-11-13 22:33:42 +0800

    Clean up code base

--- Comms/test.py
+++
@@ -1,9 +0,0 @@
-import time
-import sys
-
-for i in range(10):
-    print(f"Counter: {i}", end="\r")   # overwrite the line
-    # sys.stdout.flush()                 # force refresh
-    time.sleep(1)
-
-print("\nDone!")  
